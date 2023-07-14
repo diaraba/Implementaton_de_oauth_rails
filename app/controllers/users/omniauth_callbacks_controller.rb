2766a17b879312241318f875c4cc93a3e11008b6
diaraba <diarradiaraba9@gmail.com>
2023-07-14 21:40:33 +0000

    first commit

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,15 @@
+class Users::OmniauthCallbacksController < ApplicationController
+    def google
+        @user = User.find_for_google(request.env['omniauth.auth'])
+    
+        if @user.persisted?
+          sign_in_and_redirect @user, event: :authentication
+          set_flash_message(:notice, :success, kind: "google") if is_navigational_format?
+        else
+          session['devise.google_data'] = request.env['omniauth.auth'].except('extra')
+          redirect_to new_user_registration_url
+        end 
+    end
+
+    
+end
